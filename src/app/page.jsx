30e784c8fc5ec155ed22749aaa7002250e7673be
adnan <sirgeant10@gmail.com>
2025-03-25 00:24:29 +0600

    Add featured section.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/PageComponents/Home/Banner";
+import Featured from "@/components/PageComponents/Home/Featured";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <Featured />
     </>
   );
 }
