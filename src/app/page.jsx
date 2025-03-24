2191a4e7ac39bb112228c477107ae7e79a4a8270
adnan <sirgeant10@gmail.com>
2025-03-24 16:27:21 +0600

    Add banner to homepage.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,9 @@
+import Banner from "@/components/PageComponents/Home/Banner";
+
 export default function Home() {
-  return <h1 className="font-semibold text-sm">Hello World</h1>;
+  return (
+    <>
+      <Banner />
+    </>
+  );
 }
