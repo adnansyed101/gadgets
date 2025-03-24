db0139560bcf9c7662feca934290373b4cc2acb3
adnan <sirgeant10@gmail.com>
2025-03-25 00:44:36 +0600

    Reanme navbar header.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,19 +11,12 @@ import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import { Menu, Moon, Search } from "lucide-react";
 
 export default function Component() {
-  const links = [
-    { id: 1, title: "Home", to: "#" },
-    { id: 1, title: "Dashboard", to: "#" },
-    { id: 2, title: "About", to: "#" },
-    { id: 3, title: "Contact", to: "#" },
-  ];
-
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-white dark:border-gray-800 dark:bg-gray-950">
       <div className="container mx-auto flex h-16 max-w-6xl items-center justify-between px-4 md:px-6">
         <Link href="/" className="text-gray-700 font-semibold" prefetch={false}>
-          Clotherion
-          <span className="sr-only">Clotherion</span>
+          Gadgets
+          <span className="sr-only">Gadgets</span>
         </Link>
         <nav className="hidden items-center gap-6 text-sm font-medium md:flex">
           <Link href="/" className="nav-links-lg" prefetch={false}>
