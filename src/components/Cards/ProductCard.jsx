167d814facb56dfbe60be2b0d6ef895d3630c712
adnan <sirgeant10@gmail.com>
2025-03-25 15:57:01 +0600

    Change card layout.

--- src/components/Cards/ProductCard.jsx
+++ src/components/Cards/ProductCard.jsx
@@ -14,28 +14,32 @@ const ProductCard = ({ product }) => {
   return (
     <Card
       key={product.id}
-      className="hover:shadow-lg transition-shadow flex flex-col justify-between h-full "
+      className="hover:shadow-lg transition-shadow flex flex-col justify-between h-full p-1 bg-black"
     >
-      <div>
-        <CardHeader>
-          <CardTitle className="text-lg font-bold">{product.name}</CardTitle>
-          <CardDescription>${product.price}</CardDescription>
-        </CardHeader>
-        <CardContent className="flex justify-center">
-          <figure>
-            <img
-              src={product.image}
-              alt={product.name}
-              className="h-40 object-contain"
-            />
-          </figure>
-        </CardContent>
+      <div className="bg-white rounded-md py-4">
+        <div>
+          <CardContent className="flex justify-center">
+            <figure>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="h-40 object-contain rounded-md"
+              />
+            </figure>
+          </CardContent>
+          <CardHeader>
+            <CardTitle className="text-lg font-bold">{product.name}</CardTitle>
+            <CardDescription>${product.price}</CardDescription>
+          </CardHeader>
+        </div>
+        <CardFooter>
+          <Link href={`/product/${product._id}`}>
+            <Button variant="link" className="px-0">
+              View Details
+            </Button>
+          </Link>
+        </CardFooter>
       </div>
-      <CardFooter className="flex justify-between items-center">
-        <Link href={`/product/${product._id}`}>
-          <Button variant="link">View Details</Button>
-        </Link>
-      </CardFooter>
     </Card>
   );
 };
