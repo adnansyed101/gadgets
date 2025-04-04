e305565fdce0fcad7ba20c7d74a5bdf563f59c97
Adnan <sirgeant10@gmail.com>
2025-04-04 15:23:08 +0600

    Create post controller.

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -1,5 +1,6 @@
 import Category from "@/app/models/Category";
 import dbConnect from "@/lib/dbConnect";
+import { default as CategoryType } from "@/types/Category";
 
 export async function GET() {
   await dbConnect();
@@ -13,6 +14,33 @@ export async function GET() {
       return Response.json({
         success: false,
         message: "Error in fetching categories",
+      });
+    }
+  }
+}
+
+export async function POST(request: Request) {
+  await dbConnect();
+  const category: CategoryType = await request.json();
+
+  if (!category.name || !category.description) {
+    return Response.json({
+      success: false,
+      message: "Please provide all fields",
+    });
+  }
+
+  const newCategory = new Category(category);
+
+  try {
+    await newCategory.save();
+    return Response.json({ success: true, data: newCategory });
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log("Error in creating category: " + err.message);
+      return Response.json({
+        success: false,
+        message: "Error in creating category",
       });
     }
   }
