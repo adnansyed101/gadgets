c3554061364e11525a91e43db1b8f8525bbb0037
Adnan <sirgeant10@gmail.com>
2025-04-17 20:05:38 +0600

    Move product card to components folder.

--- src/app/(home)/components/ProductCard.tsx
+++ src/app/(home)/components/ProductCard.tsx
@@ -1,21 +1,25 @@
 import React from "react";
-import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "../../../components/ui/card";
 import Link from "next/link";
-import { Button } from "../ui/button";
+import { Button } from "../../../components/ui/button";
 import Image from "next/image";
-import Product from "@/types/Product";
 
-const ProductCard = ({ product }: { product: Product }) => {
-  const { stock, _id } = product;
+const ProductCard = () => {
+  const stock = 0;
   return (
-    <Card className="w-full max-w-sm mx-auto overflow-hidden transition-all duration-300 hover:shadow-xl hover:-translate-y-1 py-0 gap-2">
+    <Card className="w-full max-w-md mx-auto overflow-hidden transition-all duration-300 hover:shadow-xl hover:-translate-y-1 py-0 gap-2">
       {/* Product Image */}
       <CardHeader className="p-0">
-        <Link href={`/products/${_id}`}>
+        <Link href={`/`}>
           <div className="relative w-full h-48">
             <Image
-              src={product.images[0].url}
-              alt={product.name}
+              src="/dummy-image.jpg"
+              alt="Laptop"
               fill
               className="object-cover transition-transform duration-300 hover:scale-105"
               sizes="(max-width: 768px) 100vw, 33vw"
@@ -26,18 +30,13 @@ const ProductCard = ({ product }: { product: Product }) => {
       </CardHeader>
 
       {/* Product Details */}
-      <CardContent className="">
-        <Link href={`/products/${_id}`}>
+      <CardContent className="px-2">
+        <Link href={`/`}>
           <h3 className="text-lg font-semibold text-gray-900 truncate hover:text-blue-600">
-            {product.name}
+            Asus Rog Scar
           </h3>
         </Link>
-        <p className="text-sm text-gray-500 mt-1">
-          Sold by: {product.seller?.name || "Adnan"}
-        </p>
-        <p className="text-xl font-bold text-gray-800 mt-2">
-          ${product.price.toFixed(2)}
-        </p>
+        <p className="text-xl font-bold text-gray-800 mt-2">$1200</p>
         <p
           className={`text-sm mt-1 ${
             stock > 0 ? "text-green-600" : "text-red-600"
@@ -55,11 +54,7 @@ const ProductCard = ({ product }: { product: Product }) => {
           variant={stock > 0 ? "default" : "secondary"}
           asChild={stock > 0} // Wraps the button in a Link if in stock
         >
-          {stock > 0 ? (
-            <Link href={`/checkout/${_id}`}>Buy Now</Link>
-          ) : (
-            <span>Buy Now</span>
-          )}
+          {stock > 0 ? <Link href={`/`}>Buy Now</Link> : <span>Buy Now</span>}
         </Button>
       </CardFooter>
     </Card>
