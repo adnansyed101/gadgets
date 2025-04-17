0dc1f790230c518d86e2f7c2abd3f7258678e39e
Adnan <sirgeant10@gmail.com>
2025-04-17 20:18:26 +0600

    Add sections.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,17 @@
 import Banner from "./components/Banner";
+import Categories from "./components/Categories";
 import Featured from "./components/Featured";
+import NewsletterSection from "./components/NewsletterSection";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <Featured />
+      <Categories />
+      <WhyChooseUs />
+      <NewsletterSection />
     </>
   );
 }
