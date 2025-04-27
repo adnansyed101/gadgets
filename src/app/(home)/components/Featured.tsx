5928394f6b25256506e98ef94698506734761589
adnan <sirgeant10@gmail.com>
2025-04-27 16:36:01 +0600

    Change featured product layout.

--- src/app/(home)/components/Featured.tsx
+++ src/app/(home)/components/Featured.tsx
@@ -3,16 +3,14 @@ import ProductCard from "./ProductCard";
 
 const Featured = () => {
   return (
-    <section className="mt-30 container mx-auto">
+    <section className="py-24 container mx-auto">
       <div className="text-center mb-16">
-        <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight transition-colors mb-2">
-          Featured Gadgets
+        <h2 className="scroll-m-20 text-4xl font-semibold mb-2">
+          &mdash; Featured Gadgets &mdash;
         </h2>
-        <p className="leading-7 font-light">Our Best Sellers</p>
+        <p className="leading-7 font-light text-xl">Our Best Sellers</p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-2">
-        <ProductCard />
-        <ProductCard />
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
         <ProductCard />
         <ProductCard />
         <ProductCard />
